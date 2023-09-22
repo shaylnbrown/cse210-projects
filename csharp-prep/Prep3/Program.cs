00d906ca83d7c167bc50456e4c922540932c90aa
shaylnbrown <shaylnbrown@gmail.com>
2023-09-21 23:05:08 -0600

    Update

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int intAnswer = randomGenerator.Next(1, 100);
+        int intGuess = -999;  
+        while (intGuess != intAnswer)
+        {
+            Console.Write("Guess a number between 1 and 100: ");
+            intGuess = int.Parse(Console.ReadLine());
+            if (intGuess > intAnswer) Console.Write("The correct number is lower. ");
+            if (intGuess < intAnswer) Console.Write("The correct number is higher. ");
+        }      
+        Console.Write("Nailed it !");
     }
 }
