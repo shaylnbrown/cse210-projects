7195c1f9d469e89fcdc784a0fd36d66644f002c7
shaylnbrown <shaylnbrown@gmail.com>
2023-10-07 07:46:11 -0600

    First

---
+++ prove/Develop04/JournalEntry.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Develop04
+{
+    public class JournalEntry
+    {
+
+    public string _prompt="";
+    public string _answer="";
+    public DateTime _date=Convert.ToDateTime("1/1/1900");
+
+    public void CreateEntry()
+    {
+        _prompt=GetRandomPrompt();
+        Console.Write(_prompt + " ");
+        _answer = Console.ReadLine();
+        _date = DateTime.Now;
+    }
+
+    
+
+    public void ImportEntry(string textLine)
+    {
+        //Date|Prompt|Answer
+        string[] strEntry=textLine.Split("|");
+        _prompt=strEntry[1];
+        _date=Convert.ToDateTime(strEntry[0]);
+        _answer=strEntry[2];
+    }
+
+    public string ExportEntry(string textLine)
+    {
+        return _date + "|" + _prompt + "|" + _answer;
+    }
+
+
+    public string GetRandomPrompt()
+    {
+        string filename = "PromptList.txt";
+        string[] lines = System.IO.File.ReadAllLines(filename);
+        Random rnd = new Random();
+        int promptIndex = rnd.Next(0,lines.Length);
+        return lines[promptIndex];
+    }
+        
+    }
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Serialization;
+using Develop04;
 
-class Program
+public class Program
 {
+    
+    public int GetMenuChoice()
+    {
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quit");
+        Console.WriteLine("What would you like to do? ");
+        return int.Parse(Console.ReadLine());
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        int choice = 0;
+        while (choice != 5)
+        {       
+            
+            //choice = getmen
+        }
     }
+
+    
 }
