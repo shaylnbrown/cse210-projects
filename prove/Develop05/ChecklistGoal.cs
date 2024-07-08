214ab610a1ea0e0c5ec107bb1c170d02f8ab083b
shaylnbrown <shaylnbrown@gmail.com>
2024-07-08 00:11:56 -0600

    Done

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Develop05
+{
+    public class ChecklistGoal : SimpleGoal
+    {
+        int _completionBonus = 0;
+        int _stepPoints = 0;
+        int _stepTotal=0;        
+
+
+        public ChecklistGoal()
+        {
+            _stepPoints = _completionPoints;
+            Console.WriteLine("How many time doe sthis goal need to be completed for a bonus? ");
+            _stepTotal = int.Parse(Console.ReadLine());
+            Console.WriteLine("What is the bonus for accomplishing it that many times? ");
+            _completionBonus = int.Parse(Console.ReadLine());
+        }
+
+        public ChecklistGoal(string textLine) : base(textLine)
+        {
+            _stepPoints = _completionPoints;
+            string[] chunks = textLine.Split('|');
+            _stepTotal=int.Parse(chunks[5]);
+            _completionBonus = int.Parse(chunks[6]);
+        }
+
+        public override string SaveGoal()
+        {
+            return "SimpleGoal|" + _name + "|" + _description + "|" + _stepPoints + "|" + _completionTally + "|" + _stepTotal + "|" + _completionBonus;
+        }
+
+        public override string DisplayGoal()
+        {
+            string complete = " ";
+            if (_completionTally >= (_stepPoints * _stepTotal)) complete = "X";
+            int soFar = _completionTally/_stepPoints;
+            if (soFar > _stepTotal) soFar = _stepTotal;
+            return $"[{complete}] {_name} ({_description}) -- Currently Completed: {soFar}/{_stepTotal}";
+        }
+
+        public override void UpdateGoal()
+        {
+            if (_completionTally >= (_completionPoints * _stepTotal)) Console.WriteLine("You have already accomplished this goal.");
+            else 
+            {
+                _completionTally += _stepPoints;
+                if (_completionTally == (_completionPoints * _stepTotal))
+                {
+                    _completionTally += _completionBonus;
+                    Console.WriteLine($"Congradulations! You have earned {_completionPoints} points for completing this goal plus a {_completionBonus} point bonus!");
+                }
+                else
+                {
+                    Console.WriteLine($"Congradulations! You have earned {_stepPoints} points!");
+                }                
+            }
+        }
+
+
+    }
+}
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,9 +1,126 @@
 using System;
+using Develop05;
+using Microsoft.VisualBasic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        Console.Clear();
+        Console.WriteLine("Hello Develop05 World!");        
+        int totalPoints = 0;
+        List<SimpleGoal> goalList = new List<SimpleGoal>();
+        string type = "";
+        string[] goalLines =  new string[0];
+        int goalNumber = 0;
+        string goalFile="";
+        
+        int menuSelection=0;
+        int subMenuSelection=0;
+        while (menuSelection != 6)
+        {
+            //Console.Clear();
+            Console.WriteLine();
+            menuSelection = GetMenuSelection(totalPoints);
+            Console.WriteLine();
+            if (menuSelection == 1)
+            {
+                subMenuSelection=GetGoalType();
+                if (subMenuSelection==1) goalList.Add(new SimpleGoal());
+                if (subMenuSelection==2) goalList.Add(new EternalGoal());
+                if (subMenuSelection==3) goalList.Add(new ChecklistGoal());
+            } 
+
+            if (menuSelection == 2)
+            {
+                Console.WriteLine(DisplayAllGoals(goalList));                
+            }           
+
+            if (menuSelection == 3)
+            {
+                Console.Write("What is the filename for the goal file? ");
+                goalFile = Console.ReadLine();                
+                using (StreamWriter outputFile = new StreamWriter(goalFile))
+                {
+                    foreach (SimpleGoal goal in goalList) outputFile.WriteLine(goal.SaveGoal());
+                }
+                
+            }   
+
+            if (menuSelection == 4)
+            {
+                goalList.Clear();
+                Console.Write("What is the filename for the goal file? ");
+                goalFile = Console.ReadLine();
+                goalLines = System.IO.File.ReadAllLines(goalFile);                
+                foreach (string line in goalLines)
+                {
+                    type = line.Split('|')[0];
+                    if (type == "SimpleGoal") goalList.Add(new SimpleGoal(line));
+                    if (type == "EternalGoal") goalList.Add(new EternalGoal(line));
+                    if (type == "ChecklistGoal") goalList.Add(new ChecklistGoal(line));
+                }
+                totalPoints = TallyPoints(goalList);                
+                Console.WriteLine($"{goalLines.Length} goals have been loaded.");
+            }    
+
+            if (menuSelection == 5)
+            {
+                Console.WriteLine(DisplayAllGoals(goalList));
+                Console.WriteLine("Which goal did you make progress on?");
+                goalNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                goalList[goalNumber-1].UpdateGoal();                
+                totalPoints = TallyPoints(goalList);
+            }    
+        }
+    }
+
+    static private int GetGoalType()
+    {
+        Console.WriteLine("The types of goals are:");
+        Console.WriteLine("  1. Simple Goal");
+        Console.WriteLine("  2. Eternal Goal");
+        Console.WriteLine("  3. Checklist Goal");
+        Console.Write("Which type of goal would you like to create? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static private int GetMenuSelection(int total)
+    {
+        
+        Console.WriteLine($"You have {total} points");
+        Console.WriteLine();
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("  1. Create New Goal");
+        Console.WriteLine("  2. List Goals");
+        Console.WriteLine("  3. Save Goals");
+        Console.WriteLine("  4. Load Goals");
+        Console.WriteLine("  5. Record Event");
+        Console.WriteLine("  6. Quit");        
+        return int.Parse(Console.ReadLine());        
+    }
+
+    static private string DisplayAllGoals(List<SimpleGoal> goalsToDisplay)
+    {        
+        string output = "";
+        int index = 0;
+        while (index < goalsToDisplay.Count)
+        {
+            if (output != "") output += "\n";
+            output += $"{index + 1}. {goalsToDisplay[index].DisplayGoal()}";
+            index++;
+        } 
+
+        if (output == "") output = "There are no goals yet.";
+
+        return output;
+    }
+
+    static private int TallyPoints(List<SimpleGoal> goalsToTally)
+    {
+        int tally = 0;
+        foreach (SimpleGoal goal in goalsToTally) tally += goal.GetTally();
+        return tally;
     }
 }
