9d60b247cf26dc407822330cee249bda2dfb8b49
shaylnbrown <shaylnbrown@gmail.com>
2024-11-28 16:13:44 -0600

    First

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        int intGrade = int.Parse(Console.ReadLine());
+        string letter="";
+        if (intGrade >= 90) letter = "A";
+        else if (intGrade >= 80 && intGrade < 90) letter = "B";
+        else if (intGrade >= 70 && intGrade < 80) letter = "C";
+        else if (intGrade >= 60 && intGrade < 70) letter = "D";
+        else if (intGrade < 60) letter = "F";
+
+        if (letter == "A" || letter == "F") Console.Write($"You earned an {letter}.");
+        else Console.Write($" You earned a {letter}.");
+
+        if (intGrade >= 70) Console.Write("Congradulations on passing the course.");
+        else Console.Write(" I'm sure you will do better next time.");
     }
 }
