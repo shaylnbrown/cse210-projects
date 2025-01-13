f16f9193ba46b412d65ff4a706df39ce11eff648
shaylnbrown <shaylnbrown@gmail.com>
2025-01-13 09:57:56 -0600

    Prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string strFirst = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string strLast = Console.ReadLine();
+        Console.Write($"Your name is {strLast}, {strFirst} {strLast}.");
     }
 }
